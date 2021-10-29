9ab262b531e38d3308f20beb9cd3fca72ad30650
sefikkesim <sefikkesim@gmail.com>
2021-10-29 09:48:01 +0300

    first step

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const displayEl = document.querySelector("display");
 
+const acEl = document.querySelector(".ac");
+const pmEl = document.querySelector(".pm");
+const percentEl = document.querySelector(".percent");
+const multiplicationEl = document.querySelector(".multiplication");
+const subtractionEl = document.querySelector(".subtraction");
+const additionEl = document.querySelector(".addition");
+const equalEl = document.querySelector(".equal");
+
+
+const decimalEl = document.querySelector(".decimal");
+const number1El = document.querySelector(".number-1");
+const number2El = document.querySelector(".number-2");
+const number3El = document.querySelector(".number-3");
+const number4El = document.querySelector(".number-4");
+const number5El = document.querySelector(".number-5");
+const number6El = document.querySelector(".number-6");
+const number7El = document.querySelector(".number-7");
+const number8El = document.querySelector(".number-8");
+const number9El = document.querySelector(".number-9");
+const number0El = document.querySelector(".number-0");
+const numberElArray = [number0El,number1El,number2El,number3El,number4El,number5El,number6El,number7El,number8El,number9El];
+
+for (let i = 0 ;i < numberElArray.length; i++){
+    const numberEl = numberElArray[i];
+    numberEl.addEventListener("click", ()=> {
+
+    })
+}
+
+
+const updateTime = () =>{
+    const currentTime = new Date();
+
+    let currentHour = currentTime.getHours();
+    const currentMinute = currentTime.getMinutes();
+    if (currentHour > 12) {
+        currentHour -= 12
+    }
+    hourEl.textContent = currentHour.toString().padStart(2);
+    minuteEl.textContent= currentMinute.toString().padStart(2,"0");
+
+}
+setInterval(updateTime, 1000);
+updateTime();
