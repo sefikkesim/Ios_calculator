e4b7b56dcd06e8aaec4b5380a192af8d01852b27
sefikkesim <sefikkesim@gmail.com>
2021-10-31 06:02:16 +0200

    .

--- app.js
+++ app.js
@@ -1,64 +1,44 @@
-const hourEl = document.querySelector(".hour");
-const minuteEl = document.querySelector(".minute");
-const valueEl = document.querySelector(".value");
+// DOM Elements
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const valueEl = document.querySelector('.value');
 
-const acEl = document.querySelector(".ac");
-const pmEl = document.querySelector(".pm");
-const percentEl = document.querySelector(".percent");
-const multiplicationEl = document.querySelector(".multiplication");
-const subtractionEl = document.querySelector(".subtraction");
-const additionEl = document.querySelector(".addition");
-const equalEl = document.querySelector(".equal");
+const acEl = document.querySelector('.ac');
+const pmEl = document.querySelector('.pm');
+const percentEl = document.querySelector('.percent');
 
+const additionEl = document.querySelector('.addition');
+const subtractionEl = document.querySelector('.subtraction');
+const multiplicationEl = document.querySelector('.multiplication');
+const divisionEl = document.querySelector('.division');
+const equalEl = document.querySelector('.equal');
 
-const decimalEl = document.querySelector(".decimal");
-const number1El = document.querySelector(".number-1");
-const number2El = document.querySelector(".number-2");
-const number3El = document.querySelector(".number-3");
-const number4El = document.querySelector(".number-4");
-const number5El = document.querySelector(".number-5");
-const number6El = document.querySelector(".number-6");
-const number7El = document.querySelector(".number-7");
-const number8El = document.querySelector(".number-8");
-const number9El = document.querySelector(".number-9");
-const number0El = document.querySelector(".number-0");
-const numberElArray = [number0El,number1El,number2El,number3El,number4El,number5El,number6El,number7El,number8El,number9El];
-
-const getValueAsStr = () => valueEl.textContent.split(",").join(" ");
-
-const getValueAsNum = ()=>{
-    return parseFloat(getValueAsStr())
-}
-
-const handleNumberClick = (numStr)=>{
-    const currentDisplayStr = getValueAsStr();
-    if(currentDisplayStr === "0"){
-        valueEl.textContent = numStr;
-    }else{
-        valueEl.textContent = parseFloat(currentDisplayStr + numStr).toLocaleString();
-    }
-}
-
-for (let i = 0 ;i < numberElArray.length; i++){
-    const numberEl = numberElArray[i];
-    numberEl.addEventListener("click", ()=> {
-        handleNumberClick(i.toString());
-
-    })
-}
+const decimalEl = document.querySelector('.decimal');
+const number0El = document.querySelector('.number-0');
+const number1El = document.querySelector('.number-1');
+const number2El = document.querySelector('.number-2');
+const number3El = document.querySelector('.number-3');
+const number4El = document.querySelector('.number-4');
+const number5El = document.querySelector('.number-5');
+const number6El = document.querySelector('.number-6');
+const number7El = document.querySelector('.number-7');
+const number8El = document.querySelector('.number-8');
+const number9El = document.querySelector('.number-9');
 
 
-const updateTime = () =>{
-    const currentTime = new Date();
 
-    let currentHour = currentTime.getHours();
-    const currentMinute = currentTime.getMinutes();
-    if (currentHour > 12) {
-        currentHour -= 12
-    }
-    hourEl.textContent = currentHour.toString().padStart(2);
-    minuteEl.textContent= currentMinute.toString().padStart(2,"0");
+// Set up the time
+const updateTime = () => {
+  const currentTime = new Date();
 
+  let currentHour = currentTime.getHours();
+  const currentMinute = currentTime.getMinutes();
+
+  if (currentHour > 12) {
+    currentHour -= 12;
+  }
+  hourEl.textContent = currentHour.toString();
+  minuteEl.textContent = currentMinute.toString().padStart(2, '0');
 }
 setInterval(updateTime, 1000);
 updateTime();
