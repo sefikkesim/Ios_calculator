204b7bd12779f989504b368b608fdb2e117c5352
sefikkesim <sefikkesim@gmail.com>
2021-10-31 08:25:44 +0200

    .

--- app.js
+++ app.js
@@ -1,44 +1,98 @@
-// DOM Elements
-const hourEl = document.querySelector('.hour');
-const minuteEl = document.querySelector('.minute');
-const valueEl = document.querySelector('.value');
-
-const acEl = document.querySelector('.ac');
-const pmEl = document.querySelector('.pm');
-const percentEl = document.querySelector('.percent');
-
-const additionEl = document.querySelector('.addition');
-const subtractionEl = document.querySelector('.subtraction');
-const multiplicationEl = document.querySelector('.multiplication');
-const divisionEl = document.querySelector('.division');
-const equalEl = document.querySelector('.equal');
-
-const decimalEl = document.querySelector('.decimal');
-const number0El = document.querySelector('.number-0');
-const number1El = document.querySelector('.number-1');
-const number2El = document.querySelector('.number-2');
-const number3El = document.querySelector('.number-3');
-const number4El = document.querySelector('.number-4');
-const number5El = document.querySelector('.number-5');
-const number6El = document.querySelector('.number-6');
-const number7El = document.querySelector('.number-7');
-const number8El = document.querySelector('.number-8');
-const number9El = document.querySelector('.number-9');
-
-
-
-// Set up the time
-const updateTime = () => {
-  const currentTime = new Date();
-
-  let currentHour = currentTime.getHours();
-  const currentMinute = currentTime.getMinutes();
-
-  if (currentHour > 12) {
-    currentHour -= 12;
+const currentDisplayField = document.querySelector(".current-display");
+const previousDisplayField = document.querySelector(".previous-display");
+const buttonsContainer = document.querySelector(".buttons-container");
+buttonsContainer.addEventListener("click", e =>{
+  if (e.target.classList.contains("number") && currentDisplayField.innerText.length <8) {
+    if ((currentDisplayField.innerText == "" && e.target.classList.contains("number-0")) || currentDisplayField.innerText.split("")[0] == 0) {
+      currentDisplayField.innerText = e.target.innerText;
+    }    
+    else{
+      currentDisplayField.innerText += e.target.innerText;
+    }
   }
-  hourEl.textContent = currentHour.toString();
-  minuteEl.textContent = currentMinute.toString().padStart(2, '0');
-}
-setInterval(updateTime, 1000);
-updateTime();
+  if (e.target.classList.contains("decimal") && !currentDisplayField.innerText.includes(",")) {
+    currentDisplayField.innerText += e.target.innerText;
+  }
+  //*******// if (currentDisplayField.innerText.includes("0") && currentDisplayField.innerText.includes(",") ) {
+  //   currentDisplayField.innerText += e.target.innerText;
+  // }
+  if (e.target.classList.contains("operator")) {
+    const operator = e.target.innerText;
+    switch (operator) {
+      case "+":
+        if (currentDisplayField.innerText !== "") {
+          previousDisplayField.innerText = `${currentDisplayField.innerText} ${operator}`;
+          currentDisplayField.innerText = ""
+        }
+        break;
+        case "-":
+          if (currentDisplayField.innerText !== "") {
+            previousDisplayField.innerText = `${currentDisplayField.innerText} ${operator}`;
+            currentDisplayField.innerText = ""
+          }
+          break;
+        case "×":
+          if (currentDisplayField.innerText !== "") {
+            previousDisplayField.innerText = `${currentDisplayField.innerText} ${operator}`;
+            currentDisplayField.innerText = ""
+          }
+          break;
+        case "÷":
+          if (currentDisplayField.innerText !== "") {
+            previousDisplayField.innerText = `${currentDisplayField.innerText} ${operator}`;
+            currentDisplayField.innerText = ""
+          }
+        case "=":
+          if (currentDisplayField.innerText !== "") {
+            if (previousDisplayField.innerText.includes("+")) {
+              const currentNumber = Number(currentDisplayField.innerText);
+              const previousNumber = Number(previousDisplayField.innerText.split(" ")[0]);
+              currentDisplayField.innerText = currentNumber + previousNumber;
+              previousDisplayField.innerText = ""
+            }
+            if (currentDisplayField.innerText !== "") {
+              if (previousDisplayField.innerText.includes("-")) {
+                const currentNumber = Number(currentDisplayField.innerText);
+                const previousNumber = Number(previousDisplayField.innerText.split(" ")[0]);
+                currentDisplayField.innerText = currentNumber - previousNumber;
+                previousDisplayField.innerText = ""
+              }
+              if (currentDisplayField.innerText !== "") {
+                if (previousDisplayField.innerText.includes("÷")) {
+                const currentNumber = Number(currentDisplayField.innerText);
+                const previousNumber = Number(previousDisplayField.innerText.split(" ")[0]);
+                if (currentNumber == 0) {
+                  currentDisplayField.innerText = "HATA";
+                }
+                else{
+                  currentDisplayField.innerText = previousNumber / currentNumber;
+                }
+              if (currentDisplayField.innerText !== "") {
+                if (previousDisplayFiedl.innerText.includes("x")) {
+                }
+              }
+                }
+              }
+            }
+          }
+      default:
+        break;
+    }
+  }
+})
+
+// // Set up the time
+// const updateTime = () => {
+//   const currentTime = new Date();
+
+//   let currentHour = currentTime.getHours();
+//   const currentMinute = currentTime.getMinutes();
+
+//   if (currentHour > 12) {
+//     currentHour -= 12;
+//   }
+//   hourEl.textContent = currentHour.toString();
+//   minuteEl.textContent = currentMinute.toString().padStart(2, '0');
+// }
+// setInterval(updateTime, 1000);
+// updateTime();
